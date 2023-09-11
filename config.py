0476a9e60461ee01627da0072fcb9fc77a6c5e1d
CYKam-00 <98572891+CYKam-00@users.noreply.github.com>
2023-09-11 22:19:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ckqinoybtwhc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanyeikkam-employee"
+customregion = "us-east-1"
